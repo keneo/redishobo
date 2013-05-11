69f2afaaa4079c2a25a760505dc5a42d9521a59d
keneo <bartek@szabat>
2013-05-11 16:58:51 +0200

    use base vagrant box

--- init.sh
+++ init.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-mkdir /opt/redis
-mkdir /opt/redis/bin
 
-cd /opt/redis
-wget http://redis.googlecode.com/files/redis-2.6.7.tar.gz
-tar xzf redis-2.6.7.tar.gz
+apt-get install redis-server
 
-cd redis-2.6.7
-make
+# listen on all interfaces
+sed -i s/^bind/#bind/g /etc/redis/redis.conf
 
-cp src/redis-server /opt/redis/bin/redis-server
-cp src/redis-cli /opt/redis/bin/redis-cli
+service redis-server restart
 
-cp /vagrant/redis.init.d /etc/init.d/redis
-cp /vagrant/redis.conf /etc/redis.conf
-
-mkdir /var/redis
-chmod -R 777 /var/redis
-
-useradd redis
-
-/etc/init.d/redis start
+echo 'Now tailing the log...'
+tail -f /var/log/redis/redis-server.log
